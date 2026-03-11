ef4efde17e524eaf62e6ac99a8516688709b1c85
maylis imperiali <imp.mayli@gmail.com>
2026-03-11 14:31:42 +0100

    organized folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthWrapper } from './context/auth.context.jsx';
-import { useEffect } from 'react';
+
 
 createRoot(document.getElementById('root')).render(
    <AuthWrapper>
-  <StrictMode>
   <Router>
     <App />
     </Router>
-  </StrictMode>
   </AuthWrapper>
   
 )
